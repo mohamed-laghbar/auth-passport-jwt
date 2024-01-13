6f58c0d68c73ed78c7488f487877e9d61ef79c8b
mohamed-laghbar <simoghbaar@gmail.com>
2024-01-13 18:31:15 +0100

    add token methods to index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const createServer = require("./lib/server");
 const initPassportAuth = require("./lib/auth");
+const { generateToken, verifyToken } = require("./lib/token"); // Replace with the actual path to your module file
 
 module.exports = {
   createServer,
   initPassportAuth,
+  generateToken,
+  verifyToken,
 };
